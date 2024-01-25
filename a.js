8bace563831b43ada97193d83d8411a441576c96
Imran nissar <38746061+imrannissar@users.noreply.github.com>
2024-01-25 10:43:27 +0530

    asd

--- a.js
+++ a.js
@@ -1,1 +1,20 @@
-var xhr=new XMLHttpRequest;xhr.open("GET","https://ads.tiktok.com/marketing_api/api/developer/app/get/",!0),xhr.setRequestHeader("Content-type","application/json"),xhr.setRequestHeader("X-Csrftoken",""+cookie),xhr.onload=()=>{var e;let t=JSON.parse(xhr.responseText);alert(JSON.stringify(t))},xhr.send(null);
+var xhr = new XMLHttpRequest();
+var url = "https://ads.tiktok.com/marketing_api/api/developer/app/get/";
+
+// Replace 'your_csrf_token' with the actual CSRF token value
+var csrfToken = 'your_csrf_token';
+
+xhr.open("GET", url, true);
+xhr.setRequestHeader("Content-type", "application/json");
+xhr.setRequestHeader("X-Csrftoken", csrfToken);
+
+xhr.onload = function () {
+  if (xhr.status == 200) {
+    var responseData = JSON.parse(xhr.responseText);
+    alert(JSON.stringify(responseData));
+  } else {
+    alert('Error: ' + xhr.status);
+  }
+};
+
+xhr.send(null);
