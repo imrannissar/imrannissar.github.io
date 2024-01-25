e2dc8ead258621f37b995fe2b5f21f656854f073
Imran nissar <38746061+imrannissar@users.noreply.github.com>
2024-01-25 10:40:45 +0530

    asd

--- a.js
+++ a.js
@@ -1,10 +1,1 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://ads.tiktok.com/marketing_api/api/developer/app/get/', true);
-xhr.setRequestHeader('Content-type', 'application/json');
-xhr.setRequestHeader('X-Csrftoken', '' +cookie);
-xhr.onload = () => {
-   const data = JSON.parse(xhr.responseText);
-var imran= (JSON.stringify(data));
-alert(imran)
-};
-xhr.send(null);
+var xhr=new XMLHttpRequest;xhr.open("GET","https://ads.tiktok.com/marketing_api/api/developer/app/get/",!0),xhr.setRequestHeader("Content-type","application/json"),xhr.setRequestHeader("X-Csrftoken",""+cookie),xhr.onload=()=>{var e;let t=JSON.parse(xhr.responseText);alert(JSON.stringify(t))},xhr.send(null);
