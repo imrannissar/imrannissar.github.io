329279e3a72fe8c46cd17f1a239c3d0616311cd2
Imran nissar <38746061+imrannissar@users.noreply.github.com>
2024-01-25 10:51:17 +0530

    asd

--- a.js
+++ a.js
@@ -1,20 +1,4 @@
-var xhr = new XMLHttpRequest();
-var url = "https://ads.tiktok.com/marketing_api/api/developer/app/get/";
-
-// Replace 'your_csrf_token' with the actual CSRF token value
-var csrfToken = 'your_csrf_token';
-
-xhr.open("GET", url, true);
-xhr.setRequestHeader("Content-type", "application/json");
-xhr.setRequestHeader("X-Csrftoken", csrfToken);
-
-xhr.onload = function () {
-  if (xhr.status == 200) {
-    var responseData = JSON.parse(xhr.responseText);
-    alert(JSON.stringify(responseData));
-  } else {
-    alert('Error: ' + xhr.status);
-  }
-};
-
-xhr.send(null);
+fetch("https://imx.requestcatcher.com/test/")
+  .then(response => response.text())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
